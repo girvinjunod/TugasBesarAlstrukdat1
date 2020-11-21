c9acb622804c037812a26fc5d4bbb5070baa53f4
kinantanbagaspati <kinantanyolo@gmail.com>
2020-11-21 19:28:08 +0700

    Sebelum post test agama

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "boolean.h"
+#include "stack.h"
+#include "map.h"
+
+char option[];
+Map map;
+boolean play;
+
+void showMenu(){
+    printf("// Welcome to Willy wangky's fum factory!!//");
+    printf("// New game / load game / exit? //");
+    scanf("%s", option);
+    if(option == "new"){
+        map.init();
+    }else if(option == "load"){
+        map.load();
+    }else{
+        play = false;
+        printf("// Thanks for playing!!! //");
+    }
+}
+
+int main(){
+    play = true;
+    showMenu();
+    while(play){
+
+    }
+}
---
+++ map.c
@@ -0,0 +1,1 @@
+//define Map disini
