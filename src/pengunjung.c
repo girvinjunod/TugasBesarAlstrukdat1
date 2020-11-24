7d63829645188a8d0aa0475d2b1c93387617cb21
ronggurmahendra <ronggur.mwp@gmail.com>
2020-11-24 23:20:16 +0700

    adding document to pengunjung

--- src/pengunjung.c
+++ src/pengunjung.c
@@ -9,7 +9,7 @@ typedef struct tPengunjung {
 } Pengunjung;*/
 
 void generatePengunjung(int n){ //n itu jumlahnya peng7unjung yang di generate
-	Pengunjung ListPengunjung[100];
+	Pengunjung ListPengunjung[100];// ini jadi public cvasiable kalau bisa 
 	int i,j;
 	for(i = 0;i < 100; i++){
 		for (j = 0; j < 100; j++){
