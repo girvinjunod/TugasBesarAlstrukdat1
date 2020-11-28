8140af1f3c5fb22284320a633a7d0d77813e8ba7
Jauhar-Wibisono <jauhar.wibisono@gmail.com>
2020-11-29 01:25:13 +0700

    + globalvariable.h

---
+++ src/globalvariable.h
@@ -0,0 +1,26 @@
+#include "boolean.h"
+#include "graph.h"
+#include "jam.h"
+#include "listarray.h"
+#include "listlinier.h"
+#include "mainphase.h"
+#include "map.h"
+#include "matriks.h"
+#include "mesinkar.h"
+#include "mesinkata.h"
+#include "pengunjung.h"
+#include "point.h"
+#include "prepphase.h"
+#include "prioqueuePengunjung.h"
+#include "stack.h"
+#include "tree.h"
+#include "wahana.h"
+
+boolean play;
+int nbWahana;
+Wahana ActiveWahana[100];
+Tree DataWahana;
+PrioQueuePengunjung PQ[100];
+PrioQueuePengunjung Antrian;
+JAM Sekarang;
+int option;
