715e4b894e842a89bd8c23b83843d89a8a7bd651
syamilca <syamilca01@gmail.com>
2020-11-29 19:02:55 +0700

    update adt material buat wahana

--- src/wahana/wahana.c
+++ src/wahana/wahana.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "wahana.h"
 
 void PrintName(Wahana W){
@@ -11,31 +12,41 @@ void PrintName(Wahana W){
     }
 }
 
-Wahana ReadWahana(){
-    Wahana W;
-    ID(W) = ToInt(CKata); ADVKATA();
-    Name(W) = CKata; ADVKATA();
-    BuildTime(W) = ToInt(CKata); ADVKATA();
-    Resources(W) = ToInt(CKata); ADVKATA();
-    SizeX(W) = ToInt(CKata); ADVKATA();
-    SizeY(W) = ToInt(CKata); ADVKATA();
-    Duration(W) = ToInt(CKata); ADVKATA();
-    Capacity(W) = ToInt(CKata); ADVKATA();
-    Price(W) = ToInt(CKata); ADVKATA();
-    ChanceRusak(W) =  (double)ToInt(CKata)/1e6; ADVKATA();
-    return W;
+void ReadWahana(Wahana *W){
+    int i;
+    MakeEmpty(&Resource(*W));
+    strcpy(Info(Resource(*W),0),"batu");
+    strcpy(Info(Resource(*W),1),"kayu");
+    strcpy(Info(Resource(*W),2),"besi");
+    strcpy(Info(Resource(*W),3),"bata");
+    strcpy(Info(Resource(*W),4),"semen");
+    ID(*W) = ToInt(CKata); ADVKATA();
+    Name(*W) = CKata; ADVKATA();
+    BuildTime(*W) = ToInt(CKata); ADVKATA();
+    for (i = 0; i < 5; i++){
+        Value(Resource(*W),i) = ToInt(CKata);
+        ADVKATA();
+    }
+    SizeX(*W) = ToInt(CKata); ADVKATA();
+    SizeY(*W) = ToInt(CKata); ADVKATA();
+    Duration(*W) = ToInt(CKata); ADVKATA();
+    Capacity(*W) = ToInt(CKata); ADVKATA();
+    Price(*W) = ToInt(CKata); ADVKATA();
+    ChanceRusak(*W) =  (double)ToInt(CKata)/1e6; ADVKATA();
 }
 
 void PrintInfoWahana(Wahana W){
     printf("ID: %d\n", ID(W));
     printf("BuildTime: %d\n", BuildTime(W));
-    printf("Resources: %d\n", Resources(W));
+    printf("Resources needed : \n");
+    printW(Resource(W));
     printf("Ukuran: %d x %d\n", SizeX(W), SizeY(W));
     printf("Duration: %d\n", Duration(W));
     printf("Capacity: %d\n", Capacity(W));
     printf("Price: %d\n", Price(W));
     printf("Chance Rusak: %.2f\n", ChanceRusak(W));
 }
+
 void LaporanWahana(Wahana W){
     printf("Total Penghasilan: %d\n", TotalGold(W));
     printf("Total Dinaiki: %d\n", TotalRide(W));
@@ -45,10 +56,13 @@ void LaporanWahana(Wahana W){
 
 Wahana CopyWahana(Wahana W){
     Wahana Copy;
-    ID(Copy) = ID(W);
+    int i;
     Name(Copy) = Name(W);
     BuildTime(Copy) = BuildTime(W);
-    Resources(Copy) = Resources(W);
+    for (i = 0; i < 5; i++){
+        strcpy(Info(Resource(Copy),i), Info(Resource(W),i));
+        Value(Resource(W),i) = Value(Resource(W),i);
+    }
     PosX(Copy) = PosX(W);
     PosY(Copy) = PosY(W);
     SizeX(Copy) = SizeX(W);
--- src/wahana/wahana.h
+++ src/wahana/wahana.h
@@ -3,13 +3,14 @@
 
 #include "../util/boolean.h"
 #include "../mesinkata/mesinkata.h"
+#include "../material/material.h"
 
 typedef struct {
     //Statis selama program jalan
     int ID;
     Kata Name;
 	int BuildTime;
-    int Resources;
+    Material Resources;
     int SizeX;
     int SizeY;
     int Duration;
@@ -46,7 +47,7 @@ typedef struct {
 
 void PrintName(Wahana W);
 
-Wahana ReadWahana();
+void ReadWahana(Wahana *W);
 
 void PrintInfoWahana(Wahana W);
 
